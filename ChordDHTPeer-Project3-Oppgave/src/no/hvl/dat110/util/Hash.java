b45bc433601b2e3f53a18ce6d86449f3e63c9fa1
PaTrond <trond@hauge.app>
2021-04-19 17:59:29 +0200

    Moved error handling in bitSize()

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -6,71 +6,71 @@ package no.hvl.dat110.util;
  *
  */
 
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	private static BigInteger hashint; 
-	
-	public static BigInteger hashOf(String entity) {		
-		
-	
+public class Hash {
+
+	private static BigInteger hashint;
+
+	public static BigInteger hashOf(String entity) {
+
 		try {
-			
+
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			
-			byte [] entityBytes = entity.getBytes();
-			
+
+			byte[] entityBytes = entity.getBytes();
+
 			md5.update(entityBytes);
-			
+
 			byte[] digest = md5.digest();
 
 			String resultat = toHex(digest);
 			hashint = new BigInteger(resultat, 16);
-			
-		} catch(NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
-		return hashint;
-	}
-	
-	public static BigInteger addressSize() {
-		
-		MessageDigest md; 
-		
-		try {
-			 
-			md = MessageDigest.getInstance("MD5");
-			int length = md.getDigestLength();
-			int numBit = length * 8; 
-			BigInteger svar = new BigInteger("2").pow(numBit);
-			return svar; 
-			
+
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		
+
+		return hashint;
+	}
+
+	public static BigInteger addressSize() {
+
+		MessageDigest md;
+
+		try {
+
+			md = MessageDigest.getInstance("MD5");
+			int length = md.getDigestLength();
+			int numBit = length * 8;
+			BigInteger svar = new BigInteger("2").pow(numBit);
+			return svar;
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
-	
-	public static int bitSize() throws NoSuchAlgorithmException {
-		
+
+	public static int bitSize() {
+
 		int digestlen = 0;
-		
-		digestlen = MessageDigest.getInstance("MD5").getDigestLength();
-		
-	
+
+		try {
+			digestlen = MessageDigest.getInstance("MD5").getDigestLength();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		return digestlen * 8;
 	}
-	
+
 	public static String toHex(byte[] digest) {
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
