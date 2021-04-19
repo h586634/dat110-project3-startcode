c4f6cba3ead384c45f7c770829c9c9b75604f67e
Cathrine Monsen <monsen.cathrine@gmail.com>
2021-04-19 17:11:48 +0200

    Hash implementert

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -7,6 +7,7 @@ package no.hvl.dat110.util;
  */
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
@@ -18,42 +19,44 @@ public class Hash {
 	
 	public static BigInteger hashOf(String entity) {		
 		
-		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
-		// return the BigInteger
+		String resultat = " ";
+		try {
+			
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			byte [] md5HashBytes = md5.digest(entity.getBytes());
+			
+
+			resultat = toHex(md5HashBytes);
+			hashint = new BigInteger(resultat, 16);
+		} catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
 		
-		// Task: compute the address size of MD5
+		try {
+			 
+			int numberBits = bitSize();
+			double addSize = Math.pow(2, numberBits);
+			hashint = BigDecimal.valueOf(addSize).toBigInteger();
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		
-		// get the digest length
-		
-		// compute the number of bits = digest length * 8
-		
-		// compute the address size = 2 ^ number of bits
-		
-		// return the address size
-		
-		return null;
+		return hashint;
 	}
 	
-	public static int bitSize() {
+	public static int bitSize() throws NoSuchAlgorithmException {
 		
 		int digestlen = 0;
 		
-		// find the digest length
-		
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		digestlen = md5.getDigestLength();
+	
 		return digestlen*8;
 	}
 	
